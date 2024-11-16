3d42fe076ce8f155db1bc0c67815ba2afcd3f890
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-16 15:53:20 +0000

    Rename file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from llama_index.llms.ollama import Ollama
 
+# Setup model
 llm = Ollama(base_url="localhost:11434", model="gemma2")
 
 resp = llm.complete("Hi")
--- ollama.py
+++
@@ -1,6 +0,0 @@
-from llama_index.llms.ollama import Ollama
-
-llm = Ollama(base_url="localhost:11434", model="gemma2")
-
-resp = llm.complete("Hi")
-print(resp)
